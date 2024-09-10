098e2c100e92b4f06b726beb3c30cfa72e908649
mkheck13 <mkheck13@gmail.com>
2024-09-10 09:08:39 -0700

    push

--- Program.cs
+++ Program.cs
@@ -1,13 +1,17 @@
 ﻿
-Console.Clear();
+
 
 bool playAgain =true;
 string? userName;
 int userNum;
+string breakLine = "---------------------------------------------------------";
 
-
+Console.Clear();
+Console.WriteLine(breakLine);
 Console.WriteLine("Today we are going to be asking some questions.");
 Console.WriteLine("When prompted please provide your name and then the time you woke up this morning.");
+Console.WriteLine(breakLine);
+
 
 while(playAgain){
     Console.WriteLine("So, what's your name?");
@@ -28,77 +32,52 @@ while(playAgain){
         userName = Console.ReadLine();
 
         if(trueName(userName)){
+            Console.Clear();
             Console.WriteLine($"Nice to meet you {userName}.");
             break;
         }else{
+            Console.Clear();
             Console.WriteLine("Please only use letters the alphabet");
             // break;
         }
     }
+    Console.WriteLine(breakLine);
     Console.WriteLine("Now, what time did you wake up today?");
-    Console.WriteLine("Please only use 1200 or 930 format.");
+    Console.WriteLine("Please only use military time format.");
+    Console.WriteLine("Example: 0100 = 1:00am, 2300 = 11:00pm");
+    Console.WriteLine(breakLine);
     bool succcess = Int32.TryParse(Console.ReadLine(), out userNum);    
     while(true){
         if(!succcess){
+            Console.Clear();
             Console.WriteLine("That is not a number or the proper format.");
-            Console.WriteLine("Please only use 1200 or 930 format.");
+            Console.WriteLine("Please only use 1200 or 0930 format.");
             succcess = int.TryParse(Console.ReadLine(), out userNum);
         }else{
+            Console.Clear();
             Console.WriteLine($"So you woke up at {userNum}.");
             break;
         }
 
     }
-    string? userAnswer;
-    Console.WriteLine("AM or PM?");
-    static bool amOrPm(string? input){
-        if(string.IsNullOrEmpty(input)){
-            return false;
-        }
-        foreach(char b in input){
-            if(!char.IsLetter(b) && b != ' '){
-                return false;
-            }
-        }
-        return true;
-    while(true){
-
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    // Console.WriteLine("AM or PM?");
-    // string? userAnswer = Console.ReadLine();
-    // userAnswer = userAnswer?.ToUpper();
-    // while(true){
-    //     if(userAnswer == "AM"){
-    //         Console.WriteLine("Nice.");
-    //         break;
-    //     }
-    //     if(userAnswer == "PM"){
-    //         Console.WriteLine("Sweet.");
-    //         break;
-    //     }else{
-    //         Console.WriteLine("Let's stick with the AM / PM format please.");
-    //     }
-    // }
+   
     Console.Clear();
-    Console.WriteLine($"Nice to meet you {userName}. {userNum}{userAnswer} seems a bit early to me.");
+    Console.WriteLine(breakLine);
+    Console.WriteLine($"Nice to meet you {userName}. {userNum} seems a bit early to me.");
+    Console.WriteLine(breakLine);
+
+    Console.WriteLine("Would you like to play again?");
+    Console.WriteLine("Type 'yes' to play again or hit any key to exit");
+    string? userInput = Console.ReadLine();
+    userInput = userInput?.ToLower();
+
+    if(userInput == "yes"){
+        Console.WriteLine("Here we go again!!!");
+    }else{
+        playAgain = false;
+        Console.WriteLine("Til next time.");
+    }
+    
 
 
-}
 }
